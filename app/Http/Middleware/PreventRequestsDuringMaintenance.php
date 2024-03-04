10d8a3358514570e45282ef4d079947c02ad0cd0
Henry Lee <henryleeworld@gmail.com>
2024-03-05 00:38:15 +0800

    feat:初始化

---
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\MaintenanceModeBypassCookie;
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
+use Illuminate\Http\RedirectResponse;
+
+class PreventRequestsDuringMaintenance extends Middleware
+{
+    /**
+     * The URIs that should be reachable while maintenance mode is enabled.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
+
+    protected function bypassResponse(string $secret) : RedirectResponse
+    {
+        return redirect('admin')->withCookie(
+            MaintenanceModeBypassCookie::create($secret)
+        );
+    }
+}
---
+++ lang/vendor/maintenance-switch/zh_TW/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	'tooltip' => ':Action 維護模式',
+	'success' => [
+		'title' => '維護模式 :status',
+		'body' => [
+			'up' => '您的應用程式已經上線！',
+			'down' => '秘密權杖：**:secret**',
+		],
+	],
+	'error' => [
+		'title' => '出了問題。維護模式無法 :status.',
+	],
+	'activate' => '啟用',
+	'deactivate' => '關閉',
+	'activated' => '已啟用',
+	'deactivated' => '已關閉',
+	'up' => '關閉',
+	'down' => '啟用',
+];
